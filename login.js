3a2c0d7973ae1ce0310a2408e9f8e06202605a84
Bhavesh Patel <56978703+bhavesh47135@users.noreply.github.com>
2019-11-18 22:14:06 +0000

    Add files via upload

---
+++ login.js
@@ -0,0 +1,62 @@
+function login() {
+    var Username = document.getElementById("UsernameInput").value;
+
+    if (localStorage[Username] === undefined) {
+        document.getElementById("loginFailure").innerHTML = "Username not recognized.";
+        return;
+    }
+    else {
+        var userObject = JSON.parse(localStorage[Username]);
+        var password = document.getElementById("PasswordInput").value;
+        if (password === userObject.Password) {
+            document.getElementById("loginPara").innerHTML = "Welcome " + userObject.Username + "!";
+            document.getElementById("loginFailure").innerHTML = "";
+            localStorage.loggedInUser = userObject.Username;
+        }
+        else {
+            document.getElementById("loginFailure").innerHTML = "Password is incorrect.";
+        }
+    }
+}
+
+function storeUser() {
+    var userObject = {};
+    var Username = document.getElementById("UsernameInput").value;
+
+    if (Username == null || Username == "") {
+        document.getElementById("result").innerHTML = "<b>Enter a valid Username.</b>";
+    }
+    else {
+        localStorage[userObject.Username] = JSON.stringify(userObject);
+        var Password = document.getElementById("PasswordInput").value;
+        if (Password == null || Password == "") {
+            document.getElementById("result").innerHTML = "<b>Enter a valid Password.</b>"
+        }
+        else {
+            userObject.Username = document.getElementById("UsernameInput").value;
+            userObject.Password = document.getElementById("PasswordInput").value;
+            localStorage[userObject.Username] = JSON.stringify(userObject);
+            document.getElementById("result").innerHTML = "<b>Registration successful.</b>";
+            document.getElementById("UsernameInput").disabled = true;
+            document.getElementById("PasswordInput").disabled = true;
+        }
+    }
+}
+
+function proceed() {
+    if (localStorage.loggedInUser === undefined) {
+        document.getElementById("success").innerHTML = "<b>Please log in.</b>"
+    }
+    else {
+        document.getElementById("success").innerHTML = "<b>Logging in.</b>"
+        location.href="main.html"
+    }
+}
+
+function pLogin() {
+    localStorage.Type = "Parent";
+}
+
+function sLogin() {
+    localStorage.Type = "Student";
+}
